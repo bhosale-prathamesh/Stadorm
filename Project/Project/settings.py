1a1b263ca90e7253d5689ab1e924ab6b214dad90
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2022-01-07 09:39:19 +0530

    Changed Static reference

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/Stadorm/Stadorm/Project/app/static'
+STATIC_ROOT = '/home/ubuntu/Stadorm/Project/app/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
